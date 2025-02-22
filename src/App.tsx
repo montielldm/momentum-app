78da876326f8872497849c0ace942387b14c44e3
montielldm <trace.app.it@gmail.com>
2025-02-22 18:00:05 -0500

    feat(TASK-41): Add custom toast

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { Toaster } from "sonner"
 
 const queryClient = new QueryClient()
 
@@ -16,6 +17,7 @@ function App() {
         <BrowserRouter>
           <AppRouting />
         </BrowserRouter>
+        <Toaster />
       </AuthProvider>
     </QueryClientProvider>
   )
